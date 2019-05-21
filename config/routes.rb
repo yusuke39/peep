a61001884af743616ff6b03a49751f94a002de18
yusuke39 <hiranoyusuke39@gmail.com>
2019-05-21 21:15:31 +0900

    create_user_register

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :peep
+  devise_for :users, controllers:{
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'users/destroy', to: 'devise/sessions#destroy'
+  end
+  root 'peep#index'
 end
