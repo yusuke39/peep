9be9570bb9a5b0643f8d21b758bb54bcb5ca9f70
yusuke39 <hiranoyusuke39@gmail.com>
2019-05-24 14:36:46 +0900

    edit_mypage

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get 'users/destroy', to: 'devise/sessions#destroy'
+    get 'users/index', to: 'devise/sessions#index'
   end
   root 'peep#index'
   resources :address, only: [:index, :new, :create]
+  resources :mypage, only:[:index, :destroy, :edit]
 end
