b68556aea0ae72dc510f37a100b2ca7f89ce9df5
yusuke39 <hiranoyusuke39@gmail.com>
2019-05-25 12:38:07 +0900

    edit_address_register

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -5,11 +5,17 @@ class AddressController < ApplicationController
   end
   
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to controller: 'peep', action: 'index'
+    else
+      render :new
+    end
   end
 
   private
 
   def address_params
-    params.require(:address).permit(:zip_code, :prefecture, :city, :block_number, :building_name)
+    params.require(:address).permit(:zip_code, :prefecture, :city, :block_number, :building_name).merge(user_id: current_user.id)
   end
 end
