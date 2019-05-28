5155936fab40eabaae6b2d80ea08a51452dccfd4
yusuke39 <hiranoyusuke39@gmail.com>
2019-05-28 16:37:24 +0900

    delete_migration_file

--- db/migrate/20190526073344_drop_table_tweet.rb
+++
@@ -1,5 +0,0 @@
-class DropTableTweet < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :tweets
-  end
-end
--- db/migrate/20190526074022_drop_table_change_tweets.rb
+++
@@ -1,5 +0,0 @@
-class DropTableChangeTweets < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :tweets
-  end
-end
